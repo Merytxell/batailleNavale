32ff61be28c1dc1365d5e96b868c8ad151929e4e
AvilaC <avila.claire@gmail.com>
2025-03-11 16:37:19 +0100

    suppression des doublons et correction de display grid

--- bataille_navale.py
+++ bataille_navale.py
@@ -48,19 +48,14 @@ SQUARE_STATE_REPR = [' ', 'X', '#', '-']
 # +----+---+---+---+---+---+---+---+---+---+---+
 # | 10 |   |   |   |   |   |   |   |   |   |   |
 # +----+---+---+---+---+---+---+---+---+---+---+
-aircraft_carrier = {(2, 2): True, (2, 3): True, (2, 4): True, (2, 5): True, (2, 6): True}
-cruiser          = {(4, 1): True, (5, 1): True, (6, 1): True, (7, 1): True}
-destroyer        = {(5, 3): True, (6, 3): True, (7, 3): True}
-submarine        = {(5, 8): True, (5, 9): True, (5, 10): True}
-torpedo_boat     = {(9, 5): True, (9, 6): True}
-ships_list = [aircraft_carrier, cruiser, destroyer, submarine, torpedo_boat]
+
+
 
 #ajout class bateau avec les états (touché ou coulé)
-class Ship():
-    def __init__(self,name,size):
+class Ship:
+    def __init__(self,name,positions):
         self.name= name
-        self.size = size
-        self.position=[]
+        self.position={pos: True for pos in positions}
 
     def is_hit(self, shot_coord):
         if shot_coord in self.position:
@@ -75,157 +70,90 @@ class Ship():
 class Grid:
     def __init__(self):
         self.size = grid_size
-        self.ships=[]
-        self.shoot=set()
+        self.ships = []
+        self.shoot = set()
 
     def add_ship(self,ship):
         self.ships.append(ship)
 
     def ship_shoot(self, coord):
-        self.shoot.add(coord)
-        for ship in self.ships:
-            if ship.is_hit(coord):
-                print("touché !")
-                if ship.is_sunk():
-                    print(f"{ship.name}est coulé")
-                    self.ships.remove(ship)
-                return True
-            print("Manqué")
+        if coord in self.shoot:
+            print("Vous avez déjà tiré ici.")
             return False
 
+        self.shoot.add(coord)
 
+        for ship in self.ships:
+            if ship.is_hit(coord):
+                print("Touché !")
+                if ship.is_sunk():
+                    print(f"{ship.name} est coulé !")
+                    self.ships.remove(ship)
+                return True
 
-def get_ship_by_coord():
-    """Construction d'un dictionnaire permettant de connaitre l'éventuel bateau
-       présent sur chaque case de la grille.
+        print("Manqué !")
+        return False
 
-    :return: dictionnaire dont chaque clé est les coordonnées
-             d'une case d'un navire, et sa valeur le navire en question
-    """
-
-    return {ship_coord: ship for ship in ships_list
-                                 for ship_coord in ship}
+    def display_grid(self):
+     """Affichage de la grille de jeu."""
+     print("\n    " + "   ".join(LETTERS))
+     print("  +" + "---+" * self.size)
+     for line_no in range(1, self.size + 1):
+         row = f"{line_no:2} |"
+         for column_no in range(1, self.size + 1):
+             coord = (line_no, column_no)
+             if coord in self.shoot:
+                 state = "X" if any(coord in ship.position and not ship.position[coord] for ship in self.ships) else "O"
+             else:
+                 state = " "
+             row += f" {state} |"
+         print(row)
+         print("  +" + "---+" * self.size)
 
 
 def ask_coord():
-    """Demande au joueur des coordonnées de la grille de la forme 'A1' ou 'a1' et
-       la retourne sous la forme de coordonnées au format du programme :
-       ex. : (5, 3) pour 'C5'
-
-    :return: coordonnées au format du programme
-    """
-
-    # on demande des coordonnées au joueur tant qu'il n'en fournit pas de valides
-    # (ex. : 'A1', 'H8'), puis on les transforme en des coordonnées du programme :
-    # tuple (no_ligne, no_colonne)
-    valid_coord = False
-    shot_coord = None  # pour éviter un avertissement ("can be undefined")
-
+    while True:
     # ex. d'entrée attendue : 'A1'
-    player_coord = input("Entrez les coordonnées de votre tir (ex. : 'A1', 'H8') : ")
+        player_coord = input("Entrez les coordonnées de votre tir (ex. : 'A1', 'H8') : ")
 
-    if 2 <= len(player_coord) <= 3:
-        letter, number = player_coord[0], player_coord[1:]
-        letter = letter.upper()
-        try:
-            # détermination de line_no et column_no (comptés à partir de 1)
-            line_no = int(number)
-            column_no = ord(letter) - ord('A') + 1
-            if 1 <= line_no <= grid_size and letter in LETTERS:
-                valid_coord = True
-                shot_coord = (line_no, column_no)
-        except ValueError:
-            pass
-
-    if not valid_coord:
-        shot_coord = ask_coord()
-
-    return shot_coord
+        if len(player_coord) <= 2:
+            letter, number = player_coord[0], player_coord[1:]
+            if letter in LETTERS:
+                try:
+                    line_no = int(number)
+                    column_no = LETTERS.index(letter) + 1
+                    if 1 <= line_no <= grid_size:
+                        return (line_no, column_no)
+                except ValueError:
+                    pass
+        print("Coordonnée invalides, réessayez")
 
 
-def ship_is_hit(ship, shot_coord):
-    """Indique si un navire est touché par un tir aux coordonnées indiquées."""
-    return shot_coord in ship
+
+grid = Grid()
+ships_list = [
+    Ship("Porte-avions", [(2, 2), (2, 3), (2, 4), (2, 5), (2, 6)]),
+    Ship("Croiseur", [(4, 1), (5, 1), (6, 1), (7, 1)]),
+    Ship("Destroyer", [(5, 3), (6, 3), (7, 3)]),
+    Ship("Sous-marin", [(5, 8), (5, 9), (5, 10)]),
+    Ship("Torpilleur", [(9, 5), (9, 6)])
+]
 
 
-def ship_is_sunk(ship):
-    """Indique si un navire est coulé."""
-    return not any(ship.values())
+for ship in ships_list:
+    grid.add_ship(ship)
+
+# Boucle de jeu
+while grid.ships:
+    grid.display_grid()
+    coord = ask_coord()
+    grid.ship_shoot(coord)
+
+print("Félicitations ! Tous les navires ont été coulés.")
 
 
-def analyze_shot(ship, shot_coord):
-    """Analyse les conséquences d'un tir sur un navire :
-
-    - teste si le navire est touché par le tir, le signale et le mémorise alors
-    - teste si le navire est ainsi coulé, le signale dans ce cas,
-      et le supprime de la flotte
-
-    :param ship: pour lequel on regarde si le tir le concerne
-    :param shot_coord:
-    """
-    if ship_is_hit(ship, shot_coord):
-        print('Un navire a été touché par votre tir !')
-        ship[shot_coord] = False
-        if ship_is_sunk(ship):
-            print('Le navire touché est coulé !!')
-            # le navire est supprimé de la flotte
-            ships_list.remove(ship)
 
 
-def grid_square_state(coord):
-    """Retourne l'état de la case coord de la grille
-       (cf. constantes SEA, MISSED_SHOT, HIT_SHOT, SUNK_SHOT)."""
-
-    if coord in played_shots:
-        square_ship = ship_by_coord.get(coord)
-        if square_ship:
-            square_state = SUNK_SHOT if ship_is_sunk(square_ship) else HIT_SHOT
-        else:
-            square_state = MISSED_SHOT
-    else:
-        square_state = SEA
-
-    return square_state
 
 
-def display_grid():
-    """Affichage de la grille de jeu."""
 
-    print('    ', end='')
-    for x in range(grid_size):
-        letter = LETTERS[x]
-        print(' {}  '.format(letter), end='')
-    print()
-    print('  ', '+---' * grid_size + '+')
-    for line_no in range(1,  + 1):
-        print('{:>2} |'.format(line_no), end='')
-        for column_no in range(1, grid_size + 1):
-            coord = (line_no, column_no)
-            square_state = grid_square_state(coord)
-            state_str = SQUARE_STATE_REPR[square_state]
-            print(' {} |'.format(state_str), end='')
-        print()
-        print('  ', '+---' * grid_size + '+')
-
-# -------------------------------------------------------------------------- #
-# programme principal :                                                      #
-# tant que tous les navires ne sont pas coulés, on demande au joueur         #
-# d'indiquer une case où il souhaite effectuer un tir                        #
-# -------------------------------------------------------------------------- #
-
-
-played_shots = set()  # ensemble des coordonnées des tirs des joueurs
-ship_by_coord = get_ship_by_coord()
-while ships_list:
-    display_grid()
-    next_shot_coord = ask_coord()
-    played_shots.add(next_shot_coord)
-    ship_shot = ship_by_coord.get(next_shot_coord)
-    if ship_shot and ship_shot in ships_list:
-        analyze_shot(ship_shot, next_shot_coord)
-    else:
-        print("Votre tir est tombé dans l'eau")
-    print()
-
-display_grid()
-print('Bravo, vous avez coulé tous les navires')
